6481182e62f79d662bce30b9fbdd467d8e420a1f
eror2008 <d--g@ukr.net>
2022-12-03 11:19:51 +0200

    main.java was fixed. reb_br_03

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!2; br_02");
-        System.out.println("Hello world!2; br_01");
+        System.out.println("Hello world!2; reb_br_03");
+        System.out.println("Hello world!2; reb_br_03");
     }
 }
